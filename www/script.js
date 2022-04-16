b5136e9415bfbc7b92ceee045613e8e0ec87eedb
Aleksandr Kholin <a.holin@efusion.ru>
2022-04-16 19:05:04 +0300

    edits in YT-frame and added JS

---
+++ www/script.js
@@ -0,0 +1,27 @@
+const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+    animationTime = 300,
+    framesCount = 20;
+
+anchors.forEach(function(anchor) {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        let coordY = document.querySelector(anchor.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset - 15;
+
+        let scroller = setInterval(function() {
+            // считаем на сколько скроллить за 1 такт
+            let scrollBy = coordY / framesCount;
+            
+            // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+            // и дно страницы не достигнуто
+            if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+              // то скроллим на к-во пикселей, которое соответствует одному такту
+              window.scrollBy(0, scrollBy);
+            } else {
+              // иначе добираемся до элемента и выходим из интервала
+              window.scrollTo(0, coordY);
+              clearInterval(scroller);
+            }
+        });
+    }, animationTime / framesCount);
+});
